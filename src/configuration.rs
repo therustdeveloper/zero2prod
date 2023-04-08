f6cf2dcc4c59935b4331de95806b4a309b0c4087
william <william.munozr@gmail.com>
2023-04-08 09:16:26 -0500

    add configuration yaml file and postgres connection

---
+++ src/configuration.rs
@@ -0,0 +1,36 @@
+//! src/routes/configuration.rs
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    // initialize the configuration reader
+    let settings = config::Config::builder()
+        // add configuration values from a file named `configuration.yaml`
+        .add_source(
+            config::File::new("configuration.yaml", config::FileFormat::Yaml)
+        )
+        .build()?;
+    // convert the configuration values it read into Settings type
+    settings.try_deserialize::<Settings>()
+}
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,7 @@
 use std::net::TcpListener;
 use zero2prod::startup;
+use sqlx::{PgConnection, Connection};
+use zero2prod::configuration::get_configuration;
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0")
@@ -28,6 +30,11 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_string = configuration.database.connection_string();
+    let _connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
