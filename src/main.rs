66a27850bbeca42c07486f295d8ea62316227641
William Munoz <william@rustdeveloper.io>
2023-10-30 22:07:31 -0500

    going live

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ async fn main() -> std::io::Result<()> {
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
         .connect_lazy(configuration.database.connection_string().expose_secret())
-            .expect("Failed to create a Postgres connection pool.");
+        .expect("Failed to create a Postgres connection pool.");
 
     let address = format!(
         "{}:{}",
