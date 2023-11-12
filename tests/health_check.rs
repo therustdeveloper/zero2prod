83e63a305cdcdea3959a9f9d697df0e5930333d1
William Munoz <william@rustdeveloper.io>
2023-11-12 17:14:16 -0500

    email and name sanitation

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -117,7 +117,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 }
 
 #[tokio::test]
-async fn subscribe_returns_a_400_when_data_is_missing() {
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
     // Arrange
     let app = spawn_app().await;
     let client = reqwest::Client::new();
@@ -127,7 +127,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         ("", "missing both name and email"),
     ];
 
-    for (invalid_body, error_message) in test_cases {
+    for (invalid_body, description) in test_cases {
         // Act
         let response = client
             .post(&format!("{}/subscriptions", &app.address))
@@ -142,8 +142,8 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
             400,
             response.status().as_u16(),
             // Additional customised error message on test failure
-            "The API did not fail with 400 Bad Request when the payload was {}.",
-            error_message
+            "The API did not return a 400 Bad Request when the payload was {}.",
+            description
         );
     }
 }
